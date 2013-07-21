702d1ec9b3b27862202fb4efa291972fe0ed0f34
feroz <ferozkhan27@gmail.com>
2013-07-22 01:00:55 +0800

    updated views, add ui bootstrap.

--- app.js
+++ app.js
@@ -8,22 +8,31 @@ var express = require('express'),
     user = require('./routes/user'),
     http = require('http'),
     path = require('path'),
-    consolidate = require('consolidate');
+    consolidate = require('consolidate'),
+    app_name = 'NodeExpressShop',
+    redis = require('redis');
+
+var dummy_data = [
+            {"id":"1", "name": "Nike", "size": [8, 9, 10], "price": "250"},
+            {"id":"2", "name": "Puma", "size": [8, 9, 10], "price": "350"},
+            {"id":"3", "name": "Adidas", "size": [8, 9, 10], "price": "450"}
+        ];
 
 var app = express();
 app.engine('dust', consolidate.dust);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.set('app_name', app_name);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'dust');
-app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app demo data
+app.set('currency', 'USD');
+app.set('items', dummy_data);
 
 // development only
 if ('development' == app.get('env')) {
@@ -34,5 +43,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  app_name = req.app.settings.app_name;
+  items = req.app.settings.items;
+  currency = req.app.settings.currency;
+  res.render('index', { "app_name": app_name, "items" : items, "currency":  currency});
 };
